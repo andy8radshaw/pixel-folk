265539d149a3773ab50f831c8e25c8052079dbfe
andy8radshaw <andy8radshaw@gmail.com>
2021-03-12 21:46:51 +0000

    added basic grid

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,27 @@
 function init() {
   console.log('Hello there!')
+
+  // DOM Elements
+
+  const pixelGrid = document.querySelector('.pixel-grid')
+
+
+  // grid variables
+  const width = 31
+  const cellCount = width * width
+
+
+  // creating the grid
+
+  function createPixelGrid() {
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div')
+      cell.textContent = i
+      pixelGrid.appendChild(cell)
+      cell.classList.add('whole-grid')
+    }
+  }
+  createPixelGrid()
 }
 
 window.addEventListener('DOMContentLoaded', init)
